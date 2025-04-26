f3d589f51838b9958606fc54a54280e6fa5b9ccf
Aroma-Gemini <kyou11123@gmail.com>
2025-04-27 01:33:55 +0900

    Update shiimu_bot.py

--- shiimu_bot.py
+++ shiimu_bot.py
@@ -1,3 +1,8 @@
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+intents.members = True
+
 import discord
 from discord.ext import commands
 from discord import app_commands
